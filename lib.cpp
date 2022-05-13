e83d0319f27e89378b87c411bb5a2bbbbb4b9a04
FrancescaGuzzo <100522082+FrancescaGuzzo@users.noreply.github.com>
2022-05-13 12:12:56 +0200

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,33 +1,25 @@
-
 #include "lib.h"
 
-void init(char matrice[10][20]){
+void init(char mat[10][20]){
     for (int i=0; i<10; i++){
         for (int j=0; j<20; j++){
-            matrice[i][j]=' ';
+            mat[i][j]=0;
         }
     }
 }
 
-void inserimento(char nome[10][20], int contatore){
-    while(contatore<10){
-        cin >> nome[contatore];
-        contatore++;
-    }
-}
-
-void init(char array[20]){
+void init(char arr[20]){
     for (int i=0; i<20; i++){
-        array[i]=' ';
+        arr[i]=0;
     }
 }
 
-int ricerca(char matrice[10][20], char array[20]){
+int ricerca(char nome[10][20], char n[20]){
     int x;
     for (int i=0; i<10; i++){
         x=0;
         for (int j=0; j<20; j++){
-            if(matrice[i][j]==array[j]){
+            if(nome[i][j]==n[j]){
                 x++;
             }else{
                 break;
