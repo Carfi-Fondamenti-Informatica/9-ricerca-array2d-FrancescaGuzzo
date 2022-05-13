a66a2ae4fa3aca69ea1eb3e16d10604a23b777ef
FrancescaGuzzo <100522082+FrancescaGuzzo@users.noreply.github.com>
2022-05-13 12:05:50 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,24 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
 
 int main(){
-  return 0;
+    char nome[10][20];
+    init(nome);
+
+    int contatore=0;
+    inserimento(nome, contatore);
+
+    char n[20];
+    init(n);
+    cin >> n;
+
+    int a= ricerca(nome,n);
+    if(a!=-1){
+        cout<<a;
+    } else{
+        cout<<"non presente"<<endl;
+    }
+
+    return 0;
 }
